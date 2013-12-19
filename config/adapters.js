d2594ac4d441935bb6574628a73889c0cb8869bc
Jonathan Salis <jtsseneca@hotmail.com>
2013-12-19 10:01:48 -0500

    Adding postgres db config to adapters

--- config/adapters.js
+++ config/adapters.js
@@ -24,4 +24,16 @@ module.exports.adapters = {
     module: 'sails-disk'
   },
 
+  postgres: {
+    module   : 'sails-postgresql',
+    host     : process.env.PG_HOSTNAME || 'localhost',
+    port     : process.env.PG_PORT || 5432,
+    user     : process.env.PG_USER || 'postgres',
+    password : process.env.PG_PASSWORD || '',
+    database : process.env.PG_DATABASE || 'eclipse',
+    ssl: {
+      rejectUnauthorized: false
+    },
+  }
+
 };
